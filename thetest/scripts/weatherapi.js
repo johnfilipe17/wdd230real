d8e8a75d45d27bdbe92ce7ff9605fbb43246a029
johnfilipe17 <joao.f17silva@gmail.com>
2023-03-11 16:18:23 -0300

    DGH

--- thetest/scripts/weatherapi.js
+++ thetest/scripts/weatherapi.js
@@ -1,40 +1,42 @@
-// select HTML elements in the document
+// select elements that will be changed with api
+
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const url = "https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=19ef9fa5696576c4823c61b8bbfb08fc"
+const url = "https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=9713700f155de236fbe0845774b95175";
 
-async function apiFetch() {
-    try {
-      const response = await fetch(url);
-      if (response.ok) {
-        const data = await response.json();
-        console.log(data); // this is for testing the call
-        // displayResults(data);
-      } else {
-          throw Error(await response.text());
-      }
-    } catch (error) {
+async function apiFetch(){
+    try{
+        const response = await fetch(url);
+        if(response.ok){
+            const data = await response.json();
+            console.log(data); // testing the call
+            displayResults(data);
+        }else{
+            throw Error(await response.text());
+        }
+
+    }catch(error){
         console.log(error);
     }
-  }
+}
+apiFetch();
 
-async function displayResults (weatherdata) {
-    const description = weatherdata.weather[0].description.toLowerCase();
-    const capitalizeDescription = description.split("").map(word => {
+function displayResults(weatherData){
+   currentTemp.innerHTML=`<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description.toLowerCase();
+    const capDesc = desc.split(" ").map(word => {
         let firstLetter = word.charAt(0).toUpperCase();
-        return `${firstLetter} ${word.slice(1)}`;
+        return`${firstLetter}${word.slice(1)}`;
     }).join(" ");
 
-    
-    currentTemp.innerHTML = `<strong>${weatherdata.main.temp.toFixed(0)}</strong>`;
-    weatherIcon.setAttribute("src", `https://openweathermap.org/img/w/${weatherdata.weather[0].icon}.png`);
-    weatherIcon.setAttribute("alt", `${weatherdata.weather[0].description}`);
-    captionDesc.textContent = capitalizedDescription;
+    weatherIcon.setAttribute('src',iconsrc);
+    weatherIcon.setAttribute('alt',desc);
+    captionDesc.textContent=(capDesc);
+
 }
 
-apiFetch();
-  
-  
  
